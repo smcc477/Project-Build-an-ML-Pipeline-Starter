4f2e8a71e02a1bc286cbe5a22227890eaf2dc392
Steve McCarl <smcc477@wgu.edu>
2026-01-07 14:25:17 -0500

    Fix Windows-safe W&B artifact download in data_check (avoid :latest path)

--- src/data_check/conftest.py
+++ src/data_check/conftest.py
@@ -1,5 +1,7 @@
-import pytest
+import os
+
 import pandas as pd
+import pytest
 import wandb
 
 
@@ -11,61 +13,52 @@ def pytest_addoption(parser):
     parser.addoption("--max_price", action="store")
 
 
-@pytest.fixture(scope='session')
+def _download_csv(run, artifact_name: str) -> str:
+    if artifact_name is None:
+        pytest.fail("You must provide the --csv/--ref option on the command line")
+
+    artifact = run.use_artifact(artifact_name)
+
+    # IMPORTANT: use a Windows-safe root folder (avoid ':' in paths like ':latest')
+    artifact_dir = artifact.download(root="wandb_artifacts")
+
+    for fname in os.listdir(artifact_dir):
+        if fname.lower().endswith(".csv"):
+            return os.path.join(artifact_dir, fname)
+
+    pytest.fail(f"No CSV found in artifact {artifact_name}")
+
+
+@pytest.fixture(scope="session")
 def data(request):
     run = wandb.init(job_type="data_tests", resume=True)
-
-    # Download input artifact. This will also note that this script is using this
-    # particular version of the artifact
-    data_path = run.use_artifact(request.config.option.csv).file()
-
-    if data_path is None:
-        pytest.fail("You must provide the --csv option on the command line")
-
-    df = pd.read_csv(data_path)
-
-    return df
+    data_path = _download_csv(run, request.config.option.csv)
+    return pd.read_csv(data_path)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def ref_data(request):
     run = wandb.init(job_type="data_tests", resume=True)
-
-    # Download input artifact. This will also note that this script is using this
-    # particular version of the artifact
-    data_path = run.use_artifact(request.config.option.ref).file()
-
-    if data_path is None:
-        pytest.fail("You must provide the --ref option on the command line")
-
-    df = pd.read_csv(data_path)
-
-    return df
+    data_path = _download_csv(run, request.config.option.ref)
+    return pd.read_csv(data_path)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def kl_threshold(request):
-    kl_threshold = request.config.option.kl_threshold
-
-    if kl_threshold is None:
+    if request.config.option.kl_threshold is None:
         pytest.fail("You must provide a threshold for the KL test")
+    return float(request.config.option.kl_threshold)
 
-    return float(kl_threshold)
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def min_price(request):
-    min_price = request.config.option.min_price
-
-    if min_price is None:
+    if request.config.option.min_price is None:
         pytest.fail("You must provide min_price")
+    return float(request.config.option.min_price)
 
-    return float(min_price)
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def max_price(request):
-    max_price = request.config.option.max_price
-
-    if max_price is None:
+    if request.config.option.max_price is None:
         pytest.fail("You must provide max_price")
-
-    return float(max_price)
+    return float(request.config.option.max_price)
